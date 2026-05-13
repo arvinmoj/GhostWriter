d87531e801e5bfc3fd8dbd6b1bc4dfe1343d17a1
Arvin Moj <arvin@ghostwriter.app>
2026-05-13 22:03:38 +0330

    feat: add system tray with menu, icon, and event handlers

---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,47 @@
+use tauri::{
+    image::Image,
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    App,
+};
+
+pub fn create_tray(app: &App) -> Result<(), Box<dyn std::error::Error>> {
+    let quit = MenuItem::with_id(app, "quit", "Quit GhostWriter", true, None::<&str>)?;
+    let about = MenuItem::with_id(app, "about", "About GhostWriter", true, None::<&str>)?;
+    let separator = MenuItem::with_id(app, "sep", "─────────────", false, None::<&str>)?;
+
+    let menu = Menu::with_items(app, &[&about, &separator, &quit])?;
+
+    let icon_bytes = include_bytes!("../icons/icon.png");
+    let icon = Image::from_bytes(icon_bytes)?;
+
+    let _tray = TrayIconBuilder::new()
+        .icon(icon)
+        .menu(&menu)
+        .tooltip("GhostWriter - Press Cmd+Shift+R to transform text")
+        .show_menu_on_left_click(false)
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "quit" => {
+                log::info!("Quit requested from tray");
+                app.exit(0);
+            }
+            "about" => {
+                log::info!("About clicked");
+            }
+            _ => {}
+        })
+        .on_tray_icon_event(|_tray, event| {
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                log::info!("Tray icon left-clicked");
+            }
+        })
+        .build(app)?;
+
+    log::info!("System tray created");
+    Ok(())
+}
