f0b4b1928e3e57196c9807ade2fcf768db1bf837
Arvin Moj <arvin@ghostwriter.app>
2026-05-13 22:07:14 +0330

    test: add standalone encryption functions for cross-validation

---
+++ test_encryption.rs
@@ -0,0 +1,44 @@
+use base64::{engine::general_purpose::STANDARD as BASE64, Engine};
+use hostname::get;
+
+const ENCRYPTION_MAGIC: &[u8] = b"GHOSTWRITER_V1";
+
+fn get_machine_key() -> Vec<u8> {
+    let hostname = get()
+        .map(|h| h.to_string_lossy().to_string())
+        .unwrap_or_else(|_| "ghostwriter".to_string());
+    hostname.as_bytes().to_vec()
+}
+
+fn encrypt_api_key(api_key: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let machine_key = get_machine_key();
+    let encrypted: Vec<u8> = api_key
+        .as_bytes()
+        .iter()
+        .enumerate()
+        .map(|(i, b)| b ^ machine_key[i % machine_key.len()])
+        .collect();
+
+    let mut result = ENCRYPTION_MAGIC.to_vec();
+    result.extend(encrypted);
+    Ok(BASE64.encode(&result))
+}
+
+fn decrypt_api_key(encrypted: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let data = BASE64.decode(encrypted)
+        .map_err(|e| format!("Base64 decode error: {}", e))?;
+
+    if data.starts_with(ENCRYPTION_MAGIC) {
+        let encrypted_bytes = &data[ENCRYPTION_MAGIC.len()..];
+        let machine_key = get_machine_key();
+        let decrypted: Vec<u8> = encrypted_bytes
+            .iter()
+            .enumerate()
+            .map(|(i, b)| b ^ machine_key[i % machine_key.len()])
+            .collect();
+        String::from_utf8(decrypted)
+            .map_err(|e| format!("UTF-8 conversion error: {}", e))
+    } else {
+        Err("Invalid encrypted key format".into())
+    }
+}
