b653c566699510bf7df7bc95096814c0d15d5745
Arvin Moj <arvin@ghostwriter.app>
2026-05-13 22:07:04 +0330

    test: add env variable inspection and machine key derivation test

--- src-tauri/src/debug_hostname.rs
+++ src-tauri/src/debug_hostname.rs
@@ -17,4 +17,21 @@ fn main() {
             println!("hostname::get() = Err({:?})", e);
         }
     }
+
+    println!("\n=== Environment Variables ===");
+    if let Some(host) = env::var_os("HOST") {
+        println!("HOST = {:?}", host);
+    }
+    if let Some(hostname) = env::var_os("HOSTNAME") {
+        println!("HOSTNAME = {:?}", hostname);
+    }
+    if let Some(localhostname) = env::var_os("LOCALHOSTNAME") {
+        println!("LOCALHOSTNAME = {:?}", localhostname);
+    }
+
+    let machine_key = get()
+        .map(|h| h.to_string_lossy().to_string())
+        .unwrap_or_else(|_| "ghostwriter".to_string());
+    println!("\nConfig machine key: \"{}\"", machine_key);
+    println!("As bytes: {:?}", machine_key.as_bytes());
 }
